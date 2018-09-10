b0da55fcd515ad06c673296d65178f4430f3588a
manasi9307 <manasi.thakur@sjsu.edu>
2018-09-09 19:57:33 -0700

    Database configuration

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "mongoURI" : "mongodb://manasi:m123456@ds151402.mlab.com:51402/mern_shopping_list"
+}
--- server.js
+++ server.js
@@ -5,3 +5,16 @@ const mongoose = require('mongoose');
 const app = express(); //starts the app execution
 
 app.use(bodyParser.json());
+
+//DB configuration
+const db = require('./config/keys').mongoURI;
+
+//Connection to mongodb
+mongoose
+.connect(db)
+.then(() => console.log("Connection established"))
+.catch(err => console.log(err));
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
